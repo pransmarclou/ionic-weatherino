471f44487cb68598485267579af0554e15fa58fc
prans <fhranzalimorong@gmail.com>
2019-05-10 23:19:16 +0800

    update: weather service, home contents

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  weather: any;
+  location: {
+    city: string,
+    state: string
+  };
+    
+  constructor(public navCtrl: 
+    NavController,
+    private weatherProvider: WeatherProvider) {
 
   }
+
+  ionViewWillEnter() {
+    this.location = {
+      city: 'Manila',
+      state: 'Philippines'
+    }
+ 
+  this.weatherProvider.getWeather(this.location.city, this.location.state)
+    .subscribe(weather => {
+      this.weather = weather;
+     //console.log(weather);
+    });
+  } 
 
 }
--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -1,13 +1,21 @@
-import { HttpClient } from '@angular/common/http';
+import { Http  } from '@angular/http'
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map'
 
 @Injectable()
 export class WeatherProvider {
-  apiKey = '';
+  apiKey = '643be90e9569ba4de9475f69fb3931f3';
+  url;
 
-  constructor(public http: HttpClient) {
+// /http://api.openweathermap.org/data/2.5/weather?q=plaridel&units=metric&APPID=643be90e9569ba4de9475f69fb3931f3
+
+  constructor(public http: Http) {
     console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.openweathermap.org/data/2.5/weather?q='
   }
 
+  getWeather(city, state) {
+    return this.http.get(this.url + city + ',' + state + '&units=metric&APPID=' + this.apiKey)
+      .map(res => res.json());
+  }
 }
